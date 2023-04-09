e9e424299671ff3f0f61f4bc5f41e3fe0c9d94e3
RafaelCardosoBraganca <rafacardoso635@gmail.com>
2023-04-09 15:20:32 -0300

    Create ex04.py

---
+++ ex04.py
@@ -0,0 +1,8 @@
+n=input('Escreva algo: ')
+print('o tipo primitivo é',type(n))
+print('é alfa numérico?',n.isalnum())
+print('é numérico?',n.isnumeric())
+print('é alfabético?',n.isalpha())
+print('é todo minúsculo?',n.islower())
+print('é todo maiúsculo?',n.isupper())
+print('é capitalizado?',n.isascii())
