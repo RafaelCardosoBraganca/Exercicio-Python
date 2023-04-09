8142842619fda2a8ad88bd05a5f22835a7164291
RafaelCardosoBraganca <rafacardoso635@gmail.com>
2023-04-08 23:16:31 -0300

    Commitando novos exercicios

---
+++ ex02.py
@@ -0,0 +1,6 @@
+# Desafio: criar um script Python que leia o nome de 
+# uma pessoa e apresente uma mensagem de boas vindas 
+# de acordo com o valor inserido
+
+nome=input('qual seu nome?')
+print('Bem-vindo,',nome,'!!!')
