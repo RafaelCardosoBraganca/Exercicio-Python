d83e187a6fa0e96a5f22ce6c06ec55df556ee895
RafaelCardosoBraganca <rafacardoso635@gmail.com>
2023-04-09 21:04:19 -0300

    Create ex014.py

---
+++ ex014.py
@@ -0,0 +1,8 @@
+#escreva um script que leia uma temperatura em celsius
+# e converta para Fahrenheit
+
+tc=float(input('Insira uma temperatura em °C: '))
+tf= 1.8*tc+32
+tk= tc+273.15
+print('Você inseriu {:.1f}°C, que equivale à {:.1f}°F'.format(tc, tf))
+print('{:.1f}°C Também equivale à {:.1f}K'.format(tc, tk))
