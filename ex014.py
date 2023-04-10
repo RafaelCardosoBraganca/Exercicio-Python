36d09d2f2b018e98e32fe75e892a079236273fcd
RafaelCardosoBraganca <rafacardoso635@gmail.com>
2023-04-09 21:13:19 -0300

    Update ex014.py

--- ex014.py
+++ ex014.py
@@ -5,4 +5,4 @@ tc=float(input('Insira uma temperatura em °C: '))
 tf= 1.8*tc+32
 tk= tc+273.15
 print('Você inseriu {:.1f}°C, que equivale à {:.1f}°F'.format(tc, tf))
-print('{:.1f}°C Também equivale à {:.1f}K'.format(tc, tk))
+print('{:.1f}°C Também equivale à {:.1f}K'.format(tc, tk)) #plus
