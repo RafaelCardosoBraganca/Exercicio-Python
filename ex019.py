343a6c9465ab91873bf469a8a83b7485bf6d7eeb
RafaelCardosoBraganca <rafacardoso635@gmail.com>
2023-04-10 15:43:18 -0300

    ex019.py - Script de sorteio de uma lista

---
+++ ex019.py
@@ -0,0 +1,11 @@
+#crie um script que leia 4 srtings e 'sorteie' uma das 4 inseridas
+# irei fazer um script que sorteie 4 alunos, e mostre o sorteado
+from random import choice
+n1= str(input('O Primeiro Aluno  :   '))
+n2= str(input('O Segundo Aluno   :   '))
+n3= str(input('O Terceiro Aluno  :   '))
+n4= str(input('O Quanrto Aluno   :   '))
+lista= [n1, n2, n3, n4]
+chosenOne= choice(lista)
+print('='*40)
+print('O escolhido foi... {}!!'.format(chosenOne))
